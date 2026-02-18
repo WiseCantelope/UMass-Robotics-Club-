133114386f825aa2ce849ee769c7c00e1448084b
WiseCantelope <130247773+WiseCantelope@users.noreply.github.com>
2026-02-18 13:53:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// JavaScript will go here later
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+
+    if (top < windowHeight - 100) {
+      section.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
+
